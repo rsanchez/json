668a8a44884ab216bf1c0b9239037b2c53339dc8
Rob Sanchez <rsanchez@barrettnewton.com>
2011-01-09 11:26:31 -0600

    added members method. added xhr, fields, and terminate parameters. updated readme.

--- README.markdown
+++ README.markdown
@@ -6,7 +6,72 @@ Output ExpressionEngine data in JSON format.
 
 * Copy the /system/expressionengine/third_party/json/ folder to your /system/expressionengine/third_party/ folder
 
-## json:entries Usage
+## Global Parameters
+
+	xhr="yes"
+
+Set xhr to yes to only output data when an XMLHttpRequest is detected.
+
+	terminate="yes"
+
+Set terminate to yes to terminate the template and output your json immediately, with application/json content-type headers.
+
+	fields="title|url_title"
+
+Specify which fields you wish to have in the array. Separate multiple fields by a pipe character. If you do not specify fields, you will get all of the default fields' data.
+
+## Dates
+
+Date fields are in unix timestamp format, accurate to milliseconds. Use the Javascript Date object in combination with date field data:
+
+	for (i in data) {
+		var entryDate = new Date(data[i].entry_date);
+	}
+
+## Tags
+
+### json:entries
+
 	{exp:json:entries channel="news"}
 
 json:entries is a single tag, not a tag pair. Use channel:entries parameters to filter your entries.
+
+#### json:entries Default Fields
+	title
+	url_title
+	entry_id
+	channel_id
+	author_id
+	status
+	entry_date
+	edit_date
+	expiration_date
+	Plus all of the custom fields associated with that channel
+
+#### json:entries Parameters
+
+See [channel:entries parameters](http://expressionengine.com/user_guide/modules/channel/parameters.html).
+
+### json:members
+
+	{exp:json:members member_id="1|2"}
+
+json:entries is a single tag, not a tag pair. Use channel:entries parameters to filter your entries.
+
+#### json:members Parameters
+
+	member_id="1"
+
+Specify which members, by member_id, to output. Separate multiple member_id's with a pipe character.
+
+	username="admin"
+
+Specify which members, by username, to output. Separate multiple usernames with a pipe character.
+
+	group_id="1"
+
+Specify which members, by group_id, to output. Separate multiple group_id's
+
+	limit="1"
+
+Set a limit for records to retrieve.
--- system/expressionengine/third_party/json/pi.json.php
+++ system/expressionengine/third_party/json/pi.json.php
@@ -2,7 +2,7 @@
 
 $plugin_info = array(
 	'pi_name' => 'JSON',
-	'pi_version' => '1.0.0',
+	'pi_version' => '1.0.1',
 	'pi_author' => 'Rob Sanchez',
 	'pi_author_url' => 'http://barrettnewton.com/',
 	'pi_description' => 'Output ExpressionEngine data in JSON format.',
@@ -20,24 +20,136 @@ class Json
 	
 	public function entries()
 	{
-		require_once PATH_MOD.'channel/mod.channel'.EXT;
-		
-		$channel = new Channel;
-		
-		$channel->initialize();
-
-		$channel->uri = ($channel->query_string != '') ? $channel->query_string : 'index.php';
-
-		if ($channel->enable['custom_fields'] == TRUE)
+		if ($this->EE->TMPL->fetch_param('xhr') == 'yes' && ! $this->EE->input->is_ajax_request())
 		{
-			$channel->fetch_custom_channel_fields();
+			return '';
+		}
+		
+		$entries = array();
+		
+		if ($this->EE->TMPL->fetch_param('fields'))
+		{
+			$fields = explode('|', $this->EE->TMPL->fetch_param('fields'));
+		}
+		
+		if (preg_match('/t\.entry_id IN \(([\d,]+)\)/', $this->channel_sql(), $match))
+		{
+			$entry_ids = explode(',', $match[1]);
+			
+			$custom_fields = $this->EE->db->select('channel_fields.*, channels.channel_id')
+					->from('channel_fields')
+					->join('channels', 'channel_fields.group_id = channels.field_group')
+					->where_in('channels.channel_name', explode('|', $this->EE->TMPL->fetch_param('channel')))
+					->get()
+					->result_array();
+			
+			$default_fields = array(
+				't.title',
+				't.url_title',
+				't.entry_id',
+				't.channel_id',
+				't.author_id',
+				't.status',
+				't.entry_date',
+				't.edit_date',
+				't.expiration_date',
+			);
+			
+			$select = array();
+			
+			if ( ! empty($fields))
+			{
+				foreach ($default_fields as $field)
+				{
+					$key = substr($field, 2);
+					
+					if (in_array($key, $fields))
+					{
+						$select[] = $field;
+					}
+				}
+			}
+			else
+			{
+				$select = $default_fields;
+			}
+			
+			foreach ($custom_fields as &$field)
+			{
+				if (empty($fields) || in_array($field['field_name'], $fields))
+				{
+					$select[] = 'd.'.$this->EE->db->protect_identifiers('field_id_'.$field['field_id']).' AS '.$this->EE->db->protect_identifiers($field['field_name']);
+				}
+			}
+			
+			$entries = $this->EE->db->select(implode(', ', $select), FALSE)
+						->from('channel_titles t')
+						->join('channel_data d', 't.entry_id = d.entry_id')
+						->where_in('t.entry_id', $entry_ids)
+						->get()
+						->result_array();
+			
+			foreach ($entries as &$entry)
+			{
+				if (isset($entry['entry_date']))
+				{
+					$entry['entry_date'] .= '000';
+				}
+				
+				if (isset($entry['edit_date']))
+				{
+					$entry['edit_date'] = strtotime($entry['edit_date']).'000';
+				}
+				
+				if (isset($entry['expiration_date']))
+				{
+					$entry['expiration_date'] = ($entry['expiration_date']) ? $entry['expiration_date'].'000' : '';
+				}
+				
+				/*
+				foreach ($custom_fields as &$field)
+				{
+					$entry[$field['field_name']] = $this->replace_tag($entry, $field, $entry['field_id_'.$field['field_id']]);
+					
+					unset($entry['field_id_'.$field['field_id']]);
+				}
+				*/
+			}
+		}
+		
+		$this->EE->load->library('javascript');
+		
+		if ($this->EE->TMPL->fetch_param('terminate') == 'yes')
+		{
+			return $this->EE->output->send_ajax_response($entries);
+		}
+		
+		return $this->EE->javascript->generate_json($entries, TRUE);
+	}
+	
+	private function channel_sql()
+	{
+		if (empty($this->channel))
+		{
+			require_once PATH_MOD.'channel/mod.channel'.EXT;
+			
+			$this->channel = new Channel;
+		}
+		
+		$this->channel->initialize();
+
+		$this->channel->uri = ($this->channel->query_string != '') ? $this->channel->query_string : 'index.php';
+
+		if ($this->channel->enable['custom_fields'] == TRUE)
+		{
+			$this->channel->fetch_custom_channel_fields();
 		}
 
 		$save_cache = FALSE;
 
 		if ($this->EE->config->item('enable_sql_caching') == 'y')
 		{
-			if (FALSE == ($channel->sql = $channel->fetch_cache()))
+			if (FALSE == ($this->channel->sql = $this->channel->fetch_cache()))
 			{
 				$save_cache = TRUE;
 			}
@@ -45,76 +157,155 @@ class Json
 			{
 				if ($this->EE->TMPL->fetch_param('dynamic') != 'no')
 				{
-					if (preg_match("#(^|\/)C(\d+)#", $channel->query_string, $match) OR in_array($channel->reserved_cat_segment, explode("/", $channel->query_string)))
+					if (preg_match("#(^|\/)C(\d+)#", $this->channel->query_string, $match) OR in_array($this->channel->reserved_cat_segment, explode("/", $this->channel->query_string)))
 					{
-						$channel->cat_request = TRUE;
+						$this->channel->cat_request = TRUE;
 					}
 				}
 			}
 		}
 		
-		if ( ! $channel->sql)
+		if ( ! $this->channel->sql)
 		{
-			$channel->build_sql_query();
+			$this->channel->build_sql_query();
 		}
 		
-		$data = array();
-		
-		if (preg_match('/t\.entry_id IN \(([\d,]+)\)/', $channel->sql, $match))
+		return $this->channel->sql;
+	}
+	
+	public function members()
+	{
+		if ($this->EE->TMPL->fetch_param('xhr') == 'yes' && ! $this->EE->input->is_ajax_request())
 		{
-			$entry_ids = explode(',', $match[1]);
+			return '';
+		}
+		
+		if ($this->EE->TMPL->fetch_param('fields'))
+		{
+			$fields = explode('|', $this->EE->TMPL->fetch_param('fields'));
+		}
+		
+		$default_fields = array(
+			'm.member_id',
+			'm.group_id',
+			'm.username',
+			'm.screen_name',
+			'm.email',
+			'm.signature',
+			'm.avatar_filename',
+			'm.avatar_width',
+			'm.avatar_height',
+			'm.photo_filename',
+			'm.photo_width',
+			'm.photo_height',
+			'm.url',
+			'm.location',
+			'm.occupation',
+			'm.interests',
+			'm.bio',
+			'm.join_date',
+			'm.last_visit',
+			'm.last_activity',
+			'm.last_entry_date',
+			'm.last_comment_date',
+			'm.last_forum_post_date',
+			'm.total_entries',
+			'm.total_comments',
+			'm.total_forum_topics',
+			'm.total_forum_posts',
+			'm.language',
+			'm.timezone',
+			'm.daylight_savings',
+			'm.bday_d',
+			'm.bday_m',
+			'm.bday_y',
+		);
 			
-			$this->EE->db->select('channel_fields.*, channels.channel_id')
-					->from('channel_fields')
-					->join('channels', 'channel_fields.group_id = channels.field_group')
-					->where_in('channels.channel_name', explode('|', $this->EE->TMPL->fetch_param('channel')));
-			
-			$query = $this->EE->db->get();
-			
-			$select = array(
-				't.title',
-				't.url_title',
-				't.entry_id',
-				't.channel_id',
-				//'c.channel_name',
-				't.author_id',
-				't.status',
-				't.entry_date',
-				't.edit_date',
-				't.expiration_date',
-				//'d.*',
-			);
-			
-			foreach ($query->result() as $field)
+		$custom_fields = $this->EE->db->select('m_field_id, m_field_name')
+						->from('member_fields')
+						->get()
+						->result_array();
+		
+		$select = array();
+		
+		if ( ! empty($fields))
+		{
+			foreach ($default_fields as $field)
 			{
-				$select[] = 'd.'.$this->EE->db->protect_identifiers('field_id_'.$field->field_id).' AS '.$this->EE->db->protect_identifiers($field->field_name);
-				//$select[] = 'd.field_id_'.$this->EE->db->protect_identifiers('field_id_'.$field->field_id);
-			}
-			
-			$this->EE->db->select(implode(', ', $select), FALSE)
-					->from('channel_titles t')
-					//->join('channels c', 't.channel_id = c.channel_id')
-					->join('channel_data d', 't.entry_id = d.entry_id')
-					->where_in('t.entry_id', $entry_ids);
-					
-			$data = $this->EE->db->get()->result_array();
-			
-			/*
-			foreach ($data as $i => $row)
-			{
-				foreach ($query->result_array() as $field)
+				$key = substr($field, 2);
+				
+				if (in_array($key, $fields))
 				{
-					$data[$i][$field['field_name']] = $this->replace_tag($row, $field, $row['field_id_'.$field['field_id']]);
-					
-					unset($data[$i]['field_id_'.$field['field_id']]);
+					$select[] = $field;
 				}
 			}
-			*/
+		}
+		else
+		{
+			$select = $default_fields;
+		}
+		
+		foreach ($custom_fields as &$field)
+		{
+			if (empty($fields) || in_array($field['m_field_name'], $fields))
+			{
+				$select[] = 'd.'.$this->EE->db->protect_identifiers('m_field_id_'.$field['m_field_id']).' AS '.$this->EE->db->protect_identifiers($field['m_field_name']);
+			}
+		}
+		
+		$this->EE->db->select(implode(', ', $select), FALSE)
+					->from('members m')
+					->join('member_data d', 'm.member_id = d.member_id');
+		
+		if ($this->EE->TMPL->fetch_param('member_id'))
+		{
+			$this->EE->db->where_in('m.member_id', explode('|', $this->EE->TMPL->fetch_param('member_id')));
+		}
+		else if ($this->EE->TMPL->fetch_param('username'))
+		{
+			$this->EE->db->where_in('m.member_id', explode('|', $this->EE->TMPL->fetch_param('member_id')));
+		}
+		
+		if ($this->EE->TMPL->fetch_param('group_id'))
+		{
+			$this->EE->db->where_in('m.group_id', explode('|', $this->EE->TMPL->fetch_param('group_id')));
+		}
+		
+		if ($this->EE->TMPL->fetch_param('limit'))
+		{
+			$this->EE->db->limit($this->EE->TMPL->fetch_param('limit'));
+		}
+		
+		$members = $this->EE->db->get()->result_array();
+		
+		$date_fields = array(
+			'join_date',
+			'last_visit',
+			'last_activity',
+			'last_entry_date',
+			'last_comment_date',
+			'last_forum_post_date'
+		);
+		
+		foreach ($members as &$member)
+		{
+			foreach ($date_fields as $field)
+			{
+				if (isset($member[$field]))
+				{
+					$member[$field] = ($member[$field]) ? $member[$field].'000' : '';
+				}
+			}
 		}
 		
 		$this->EE->load->library('javascript');
 		
-		$this->EE->output->send_ajax_response($data);
+		if ($this->EE->TMPL->fetch_param('terminate') == 'yes')
+		{
+			return $this->EE->output->send_ajax_response($members);
+		}
+		
+		return $this->EE->javascript->generate_json($members, TRUE);
 	}
 	
 	public static function usage()
@@ -124,6 +315,8 @@ class Json
 {exp:json:entries channel="news"}
 
 {exp:json:entries channel="products" search:product_size="10"}
+
+{exp:json:members member_id="1"}
 <?php
 		$buffer = ob_get_contents();
 		      
