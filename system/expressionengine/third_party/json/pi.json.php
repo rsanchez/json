f3207dcf16770238b67804555709d9f909109f1a
Stewart McKee <stewart@theizone.co.uk>
2014-11-04 11:48:31 +0000

    added JSON_NUMERIC_CHECK to json_encode

--- system/expressionengine/third_party/json/pi.json.php
+++ system/expressionengine/third_party/json/pi.json.php
@@ -947,8 +947,10 @@ class Json
       $response = array(ee()->TMPL->fetch_param('root_node') => $response);
     }
 
+    print_r($response);
+
     $response = function_exists('json_encode')
-      ? json_encode($response)
+      ? json_encode($response, JSON_NUMERIC_CHECK)
       : ee()->javascript->generate_json($response, TRUE);
 
     if ( ! is_null($callback))
