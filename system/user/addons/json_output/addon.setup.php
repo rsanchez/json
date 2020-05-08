94d285f3fc0fe1aaa9053d07f3d37ada6121bf13
Zignature <siegert.naber@gmail.com>
2020-05-08 02:13:04 +0200

    Create addon.setup.php

---
+++ system/user/addons/json_output/addon.setup.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * 
+ * This is a fork of the Json plugin by rsanchez.
+ * https://github.com/rsanchez/json
+ * 
+ * This version is compatible with EE3, EE4 and EE5.
+ * 
+ */
+return array(
+	'author' => 'Zignature',
+	'author_url' => 'https://github.com/Zignature/ee-json-output',
+	'description' => 'Output ExpressionEngine channel entries in JSON format.',
+	'docs_url' => 'https://github.com/Zignature/ee-json-output',
+	'name' => 'EE JSON Output',
+	'settings_exist' => false,
+	'version' => '1.0.0',
+	'namespace'   => 'json_output/json_output'
+);
